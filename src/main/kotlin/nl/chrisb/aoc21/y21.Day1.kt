dbf66754e21ed908d1382d3da94d8fb5ea30f490
Chris <chirssel@gmail.com>
2021-12-01 10:10:59 +0100

    Solution: Add 2021 day 1 solution

---
+++ src/main/kotlin/nl/chrisb/aoc21/y21.Day1.kt
@@ -0,0 +1,20 @@
+package nl.chrisb.aoc21
+
+import nl.chrisb.aoc21.common.Solution
+import nl.chrisb.aoc21.common.toInts
+
+fun main() {
+    Solution(2021, 1)
+        .part1 {
+            lines.toInts()
+                .zipWithNext()
+                .count { it.first < it.second }
+        }
+        .part2 {
+            val ints = lines.toInts()
+            ints.dropLast(2)
+                .mapIndexed { i, c -> c + ints[i + 1] + ints[i + 2] }
+                .zipWithNext()
+                .count { it.first < it.second }
+        }
+}
