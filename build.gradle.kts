fb6ef9ecc5e0638101a86a9d0088005b076238b6
Chris <chirssel@gmail.com>
2021-12-05 11:59:52 +0100

    Meta: Convert to a multiplatform project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.serialization") version "1.5.31"
+    kotlin("multiplatform") version "1.5.30"
 }
 
 group = "nl.chrisb"
@@ -12,12 +9,25 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
-    implementation("com.charleskorn.kaml:kaml:0.37.0")
-    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta3")
-}
+kotlin {
+    jvm {
+        withJava()
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val jvmMain by getting
+    }
 }
