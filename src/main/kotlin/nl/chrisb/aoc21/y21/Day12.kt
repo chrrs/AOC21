e8f47132138c60c0540b5aa29d6248aadc883308
Chris <chirssel@gmail.com>
2021-12-12 11:33:59 +0100

    Solution: Remove some unnecessary code

--- src/main/kotlin/nl/chrisb/aoc21/y21/Day12.kt
+++ src/main/kotlin/nl/chrisb/aoc21/y21/Day12.kt
@@ -13,7 +13,7 @@ fun main() {
                 connections.getOrPut(parts[1]) { mutableListOf() }.add(parts[0])
             }
 
-            findPaths(connections, visited = setOf("start"), "start").size
+            findPaths(connections, visited = setOf("start"), "start", true).size
         }
         .part2 {
             val connections = mutableMapOf<String, MutableList<String>>()
@@ -24,29 +24,11 @@ fun main() {
                 connections.getOrPut(parts[1]) { mutableListOf() }.add(parts[0])
             }
 
-            findPathsTwice(connections, visited = setOf("start"), "start", false).size
+            findPaths(connections, visited = setOf("start"), "start", false).size
         }
 }
 
-fun findPaths(connections: Map<String, List<String>>, visited: Set<String>, loc: String): Set<List<String>> {
-    if (loc == "end") {
-        return setOf(listOf(loc))
-    }
-
-    val out = mutableSetOf<List<String>>()
-    connections[loc]?.forEach { neighbor ->
-        if (neighbor.lowercase() == neighbor && visited.contains(neighbor)) {
-            return@forEach
-        }
-
-        out.addAll(findPaths(connections, visited + setOf(loc), neighbor)
-            .map { listOf(loc) + it })
-    }
-
-    return out
-}
-
-fun findPathsTwice(
+fun findPaths(
     connections: Map<String, List<String>>,
     visited: Set<String>,
     loc: String,
@@ -60,14 +42,14 @@ fun findPathsTwice(
     connections[loc]?.forEach { neighbor ->
         if (neighbor.lowercase() == neighbor && visited.contains(neighbor)) {
             if (!twice && neighbor != "start") {
-                out.addAll(findPathsTwice(connections, visited + setOf(loc), neighbor, true)
+                out.addAll(findPaths(connections, visited + setOf(loc), neighbor, true)
                     .map { listOf(loc) + it })
             }
 
             return@forEach
         }
 
-        out.addAll(findPathsTwice(connections, visited + setOf(loc), neighbor, twice)
+        out.addAll(findPaths(connections, visited + setOf(loc), neighbor, twice)
             .map { listOf(loc) + it })
     }
 
